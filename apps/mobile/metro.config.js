106e230b098bec96a9811e04ea7e68e060680b4c
minhtam <minhtamnghp03@gmail.com>
2025-09-19 12:52:37 +0700

    fix/config react native plugin

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,18 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  projectRoot: __dirname,
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../../packages'),
+  ],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, '../../node_modules'),
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
---
+++ apps/mobile/react-native.config.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+  root: path.resolve(__dirname, '../..'),
+  reactNativePath: path.resolve(__dirname, '../../node_modules/react-native'),
+};
